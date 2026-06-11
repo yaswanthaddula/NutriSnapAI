6f71e8442c2b7f7fb75a5bff81a6db8b57b44829
yaswanthaddula <yaswanthaddula@gmail.com>
2026-06-11 19:54:38 +0530

    Fix icon dimensions by setting sizes to any

--- app/+html.tsx
+++ app/+html.tsx
@@ -9,9 +9,9 @@ export default function Root({ children }: PropsWithChildren) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no" />
         
-        <link rel="manifest" href="/manifest.json?v=2" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=2" />
-        <link rel="icon" type="image/png" href="/favicon.png?v=2" />
+        <link rel="manifest" href="/manifest.json?v=3" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=3" />
+        <link rel="icon" type="image/png" href="/favicon.png?v=3" />
         
         <meta name="theme-color" content="#00C853" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
